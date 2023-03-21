24a441d3cd2308efa2706ab70919b2142973252d
Varu98 <vardaan@wisdomtreeventures.com>
2023-03-21 12:30:18 +0530

    todo : auth needs to be modular and from docs not the react2025 docs

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3WvlYQyTufGxyhRaFOk6NYWp5BXxv0Hg",
+  authDomain: "auth-app-5e306.firebaseapp.com",
+  projectId: "auth-app-5e306",
+  storageBucket: "auth-app-5e306.appspot.com",
+  messagingSenderId: "1014146908522",
+  appId: "1:1014146908522:web:ad9358793afbfb91cae513",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
